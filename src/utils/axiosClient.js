c8cf46eb235e783c8ae4fe21bfa48dc4346bc2a0
Dipesh Yadav <122032063+dipeshyadav331@users.noreply.github.com>
2023-09-03 16:54:39 +0530

    Update axiosClient.js

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -9,12 +9,12 @@ import store from '../redux/store';
 import { setLoading, showToast } from "../redux/slices/appConfigSlice";
 import { TOAST_FAILURE } from "../App";
 
-let baseURL = 'http://localhost:4000/';
-// console.log('env is ', process.env.NODE_ENV);
+let baseURL = process.env.REACT_APP_SERVER_BASE_URL
+console.log('env is ', process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production') {
     baseURL = process.env.REACT_APP_SERVER_BASE_URL
 }
-
+console.log(baseURL);
 export const axiosClient = axios.create({
     baseURL,
     withCredentials: true,
